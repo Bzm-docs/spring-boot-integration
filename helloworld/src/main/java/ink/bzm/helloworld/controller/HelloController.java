18f1ca866447dffc64851405be27f5d648284a5d
孤鹊 <2794127547@qq.com>
2021-07-02 09:58:43 +0800

    updata

---
+++ helloworld/src/main/java/ink/bzm/helloworld/controller/HelloController.java
@@ -0,0 +1,20 @@
+package ink.bzm.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author: buzhengmiao    docs.bzm.ink
+ * @time: 2021/7/2 9:52
+ */
+@Controller
+public class HelloController {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello World!";
+	}
+}
+
