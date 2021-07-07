2bb181314bbbdf8b8b0eca7c7d44379ef78b8ea3
孤鹊 <2794127547@qq.com>
2021-07-07 14:38:17 +0800

    spring-boot-data-mybatis 更新

---
+++ spring-boot-data-mybatis/src/main/java/ink/bzm/bootdatamybatis/SpringBootDataMybatisApplication.java
@@ -0,0 +1,13 @@
+package ink.bzm.bootdatamybatis;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataMybatisApplication.class, args);
+	}
+}
---
+++ spring-boot-data-mybatis/src/main/java/ink/bzm/bootdatamybatis/dao/UserDao.java
@@ -0,0 +1,24 @@
+package ink.bzm.bootdatamybatis.dao;
+
+import ink.bzm.bootdatamybatis.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+/**
+ * @author: buzhengmiao
+ * @time: 2021/7/5 16:11
+ */
+@Mapper
+public interface UserDao {
+	List<User> findAll();
+
+	User findById(Long id);
+
+	void save(User user);
+
+	void update(User user);
+
+	void delete(Long id);
+}
