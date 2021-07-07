bb483efc6d43f8461688a97581bf8d94aab944d6
孤鹊 <2794127547@qq.com>
2021-07-07 11:08:28 +0800

    updata

---
+++ Spring-boot-redis/src/main/java/ink/bzm/springBootRedis/SpringBootRedisApplication.java
@@ -0,0 +1,16 @@
+package ink.bzm.springBootRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: buzhengmiao    docs.bzm.ink
+ * @time: 2021/7/7 10:54
+ */
+@SpringBootApplication
+public class SpringBootRedisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisApplication.class, args);
+
+	}
+}
